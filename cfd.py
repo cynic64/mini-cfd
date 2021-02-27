9ec82c101f3f047b1b49f3e8c72403e6346e7c25
Nicholas Schweitzer <f3bl@gmx.com>
2021-02-27 17:15:02 +0100

    works decently, can't handle turbulence tho

--- cfd.py
+++ cfd.py
@@ -2,39 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import common
 
-# Returns array with shape (m-2,n-2)
-def poisson_rhs(u, v, rho, dx, dy, dt):
-        u_diff_x = (u[1:-1,2:]-u[1:-1,:-2]) / (2*dx)
-        u_diff_y = (u[2:,1:-1]-u[:-2,1:-1]) / (2*dy)
-
-        v_diff_x = (v[1:-1:,2:]-v[1:-1,:-2]) / (2*dx)
-        v_diff_y = (v[2:,1:-1]-v[:-2,1:-1]) / (2*dy)
-
-        rhs = (u_diff_x + v_diff_y) / dt \
-                - u_diff_x**2 - 2 \
-                - 2*u_diff_y*v_diff_x \
-                - v_diff_y**2
-
-        return rho * rhs
-
-# Returns array with shape (m+2,n+2)
-# If given the previous pressure, it will be more accurate
-def poisson_solve(p, rhs, dx, dy):
-        for i in range(0, 100):
-                horiz_part = (p[1:-1,2:] + p[1:-1,:-2]) * dy**2
-                vert_part = (p[2:,1:-1] + p[:-2,1:-1]) * dx**2
-
-                p[1:-1,1:-1] = (horiz_part + vert_part - (rhs * dx**2 * dy**2)) \
-                        / (2 * dx**2 + 2 * dy**2)
-
-                # Derivatives at borders should be 0
-                p[0,:] = p[1,:]
-                p[-1,:] = 0
-                p[:,0] = p[:,1]
-                p[:,-1] = p[:,-2]
-
-        return p
-
 # Returns (m, n), (m, n)
 def next_given_pressure(u, v, p, rho, nu, dx, dy, dt):
         u_middle = u[1:-1,1:-1]
@@ -68,7 +35,7 @@ def next_given_pressure(u, v, p, rho, nu, dx, dy, dt):
 
 def apply_velocity_bc(u, v):
         u[0,:] = 0
-        u[-1,:] = 100
+        u[-1,:] = 10
         u[:,0] = 0
         u[:,-1] = 0
         v[0,:] = 0
@@ -77,20 +44,22 @@ def apply_velocity_bc(u, v):
         v[:,-1] = 0
 
 # Mesh
-nx = 100
-ny = 100
-Lx = 1
-Ly = 1
+nx = 50
+ny = 50
+Lx = 2
+Ly = 2
 dx = Lx / (nx - 1)
 dy = Ly / (ny - 1)
 x_points = np.linspace(0, Lx, nx)
 y_points = np.linspace(0, Ly, ny)
 x_mesh, y_mesh = np.meshgrid(x_points, y_points)
+# Each thing is 1/200=.005 across
+# Top velocity is 10, dt = 0.0001 --> 0.001 each time step
 
 # Constants
 rho = 1
 nu = 0.1
-dt = 0.0001
+dt = 0.001
 
 # Velocity
 u = x_mesh**2
@@ -99,17 +68,22 @@ v = np.zeros(y_mesh.shape)
 apply_velocity_bc(u, v)
 pressure = np.zeros(u.shape)
 
-for i in range(100):
-        rhs = poisson_rhs(u, v, rho, dx, dy, dt)
-        pressure = poisson_solve(pressure, rhs, dx, dy)
+for i in range(1001):
+        print(i)
+        rhs = common.poisson_rhs(u, v, rho, dx, dy, dt)
+        pressure = common.poisson_solve(pressure, rhs, dx, dy)
         u, v = next_given_pressure(u, v, pressure, rho, nu, dx, dy, dt)
         divergence = common.divergence(u, v, dx, dy)
 
-plt.imshow(divergence, cmap=plt.cm.coolwarm, origin='lower', extent=(0, Lx, 0, Ly))
-plt.colorbar()
-speed = np.sqrt(u**2+v**2)
-lw = 5*speed/speed.max()
-plt.streamplot(x_mesh, y_mesh, u, v)
-#plt.quiver(x_mesh[::2], y_mesh[::2], u[::2], v[::2])
+        if i % 1000 == 0:
+                plt.imshow(divergence, cmap=plt.cm.coolwarm, origin='lower', extent=(0, Lx, 0, Ly))
+                plt.colorbar()
+                speed = np.sqrt(u**2+v**2)
+                lw = 5*speed/speed.max()
+                plt.streamplot(x_mesh, y_mesh, u, v, linewidth=lw)
+                plt.quiver(x_mesh[::4], y_mesh[::4], u[::4], v[::4])
+                plt.savefig(f'img/{i}.png', dpi=300)
+                plt.clf()
+
 plt.show()
 
