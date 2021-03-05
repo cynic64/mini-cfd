ac4e71c23f50485af6b1dffdba1715f257d7bb93
Nicholas Schweitzer <f3bl@gmx.com>
2021-03-05 08:00:20 +0100

    Upwinding convection for u and v!

---
+++ common.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+# Returns array with shape (m-2,n-2)
+# Uses central differences
+def divergence(u, v, dx, dy):
+        u_diff_x = (u[1:-1,2:] - u[1:-1,:-2]) / (2*dx)
+        v_diff_y = (v[2:,1:-1] - v[:-2,1:-1]) / (2*dy)
+
+        return u_diff_x + v_diff_y
+
+# Returns array with shape (m-2,n-2)
+def laplacian(u, dx, dy):
+        u_diff2_x = (u[1:-1,2:] - 2*u[1:-1,1:-1] + u[1:-1,:-2]) / dx**2
+        u_diff2_y = (u[2:,1:-1] - 2*u[1:-1,1:-1] + u[:-2,1:-1]) / dy**2
+
+        return u_diff2_x + u_diff2_y
+
+# Returns array with shape (m-2,n-2)
+def poisson_rhs(u, v, rho, dx, dy, dt):
+        u_diff_x = (u[1:-1,2:]-u[1:-1,:-2]) / (2*dx)
+        u_diff_y = (u[2:,1:-1]-u[:-2,1:-1]) / (2*dy)
+
+        v_diff_x = (v[1:-1:,2:]-v[1:-1,:-2]) / (2*dx)
+        v_diff_y = (v[2:,1:-1]-v[:-2,1:-1]) / (2*dy)
+
+        rhs = (u_diff_x + v_diff_y) / dt \
+                - u_diff_x**2 - 2 \
+                - 2*u_diff_y*v_diff_x \
+                - v_diff_y**2
+
+        return rho * rhs
+
+# Returns array with shape (m+2,n+2)
+# If given the previous pressure, it will be more accurate
+def poisson_solve(p, rhs, dx, dy, target_diff=0.0001):
+        for i in range(1, 100000):
+                horiz_part = (p[1:-1,2:] + p[1:-1,:-2]) * dy**2
+                vert_part = (p[2:,1:-1] + p[:-2,1:-1]) * dx**2
+
+                p_next = np.ones_like(p)
+
+                p_next[1:-1,1:-1] = (horiz_part + vert_part - (rhs[1:-1,1:-1] * dx**2 * dy**2)) \
+                        / (2 * dx**2 + 2 * dy**2)
+
+                # Derivatives at borders should be 0
+                p_next[0,:] = p_next[1,:]
+                p_next[-1,:] = p_next[-2,:]
+                p_next[:,0] = p_next[:,1]
+                p_next[:,-1] = p_next[:,-2]
+
+                diff = np.sum(np.abs(p_next - p)) / np.sum(np.abs(p_next))
+                if diff < target_diff:
+                        break
+
+                p = p_next
+        print(f'Took {i} iterations.')
+
+        return p
+
--- mit_cfd.py
+++ mit_cfd.py
@@ -10,18 +10,48 @@ def average_velocities(u, v):
         return u_avg, v_avg
 
 def convect(u, v, dx, dy, dt):
-        # First: u*u_x
+        ## Changing u
+        # u*u_x
         u_right_avg = (u[1:-1,2:] + u[1:-1,1:-1]) / 2
         u_left_avg = (u[1:-1,:-2] + u[1:-1,1:-1]) / 2
-
         u_diff_x_forward = (u[1:-1,2:] - u[1:-1,1:-1]) / dx
         u_diff_x_backward = (u[1:-1,1:-1] - u[1:-1,:-2]) / dx
         # Only use forward if u is negative
-        u_forward_coef = np.clip(u_right_avg, None, 0)
+        u_diff_x_forward_coef = np.clip(u_right_avg, None, 0)
         # Only use backward if u is positive
-        u_backward_coef = np.clip(u_left_avg, 0, None)
+        u_diff_x_backward_coef = np.clip(u_left_avg, 0, None)
+        uu_x = u_diff_x_forward_coef*u_diff_x_forward + u_diff_x_backward_coef*u_diff_x_backward
 
-        u[1:-1,1:-1] -= dt * (u_forward_coef*u_diff_x_forward + u_backward_coef*u_diff_x_backward)
+        # v*u_y
+        v_down_avg = (v[:-1,1:-2] + v[:-1,2:-1]) / 2
+        v_up_avg = (v[1:,1:-2] + v[1:,2:-1]) / 2
+        u_diff_y_forward = (u[2:,1:-1] - u[1:-1,1:-1]) / dy
+        u_diff_y_backward = (u[1:-1,1:-1] - u[:-2,1:-1]) / dy
+        u_diff_y_forward_coef = np.clip(v_down_avg, None, 0)
+        u_diff_y_backward_coef = np.clip(v_up_avg, 0, None)
+        vu_y = u_diff_y_forward_coef*u_diff_y_forward + u_diff_y_backward_coef*u_diff_y_backward
+
+        ## Changing v
+        # v*v_y
+        v_down_avg = (v[2:,1:-1] + v[1:-1,1:-1]) / 2
+        v_up_avg = (v[1:-1,1:-1] + v[:-2,1:-1]) / 2
+        v_diff_y_forward = (v[2:,1:-1] - v[1:-1,1:-1]) / dy
+        v_diff_y_backward = (v[1:-1,1:-1] - v[:-2,1:-1]) / dy
+        v_diff_y_forward_coef = np.clip(v_down_avg, None, 0)
+        v_diff_y_backward_coef = np.clip(v_up_avg, 0, None)
+        vv_y = v_diff_y_forward_coef*v_diff_y_forward + v_diff_y_backward_coef*v_diff_y_backward
+
+        # u*v_y
+        u_right_avg = (u[1:-2,1:] + u[2:-1,1:]) / 2
+        u_left_avg = (u[1:-2,:-1] + u[2:-1,:-1]) / 2
+        v_diff_x_forward = (v[1:-1,2:] - v[1:-1,1:-1]) / dx
+        v_diff_x_backward = (v[1:-1,1:-1] - v[1:-1,:-2]) / dx
+        v_diff_x_forward_coef = np.clip(u_right_avg, None, 0)
+        v_diff_x_backward_coef = np.clip(u_left_avg, None, 0)
+        uv_x = v_diff_x_forward_coef*v_diff_x_forward + v_diff_y_forward_coef*v_diff_y_forward
+
+        u[1:-1,1:-1] -= dt * (uu_x + vu_y)
+        v[1:-1,1:-1] -= dt * (vv_y + uv_x)
 
 # Apply once to each dimension in the velocity field
 def apply_viscosity(u, nu, dx, dy, dt):
@@ -31,6 +61,16 @@ def apply_viscosity(u, nu, dx, dy, dt):
 
         for iter in range(100):
                 u[1:-1,1:-1] = (rhs[1:-1,1:-1] + dt*nu*common.laplacian(u.copy(), dx, dy)) / div
+
+def apply_bc(u, v):
+        u[0,:] = 0
+        u[-1,:] = 0
+        u[:,0] = 0
+        u[:,-1] = 0
+        v[0,:] = 0
+        v[-1,:] = 0
+        v[:,0] = 0
+        v[:,-1] = 0
 
 # Setup mesh
 len_x, len_y = 2, 2
@@ -52,24 +92,17 @@ nu = 0.1
 
 # U: (ny+2, nx+1)
 # V: (ny+1, nx+2)
-u = np.ones((ny+2, nx+1))
-v = np.zeros((ny+1, nx+2))
-u[10,40:50] = 10
+u = np.sin(u_x_mesh**2 + u_y_mesh**2)
+v = np.cos(v_x_mesh**2 + v_y_mesh**2)
+apply_bc(u, v)
+
+start_time = 0
+end_time = time.time()
 
 for i in range(1000001):
-        start_time = time.time()
-        print(i)
-
-        convect(u, v, dx, dy, dt)
-        #apply_viscosity(u, nu, dx, dy, dt)
-        #apply_viscosity(v, nu, dx, dy, dt)
-
         u_avg, v_avg = average_velocities(u, v)
         speed = np.sqrt(u_avg**2+v_avg**2)
-        dt = min(0.1, 0.1 * min(dx, dy) / np.max(speed))
         print(f'Max speed: {np.max(speed)}, dt: {dt}')
-
-        end_time = time.time()
 
         if i % 30 == 0 or end_time - start_time > 1:
                 divergence = common.divergence(u_avg, v_avg, dx, dy)
@@ -81,3 +114,13 @@ for i in range(1000001):
                 plt.quiver(x_mesh, y_mesh, u_avg, v_avg, scale=200)
                 plt.savefig(f'img/{i:07}.png', dpi=300)
                 plt.clf()
+
+        start_time = time.time()
+        print(i)
+
+        convect(u, v, dx, dy, dt)
+        #apply_viscosity(u, nu, dx, dy, dt)
+        #apply_viscosity(v, nu, dx, dy, dt)
+
+        end_time = time.time()
+
